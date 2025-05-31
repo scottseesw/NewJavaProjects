f117098c8935a35f0a45a191ff1f468236c0ee05
ScottChris <scottchris@MacBook-Air.lan>
2025-05-31 11:18:29 -0400

    Add WhileLoopExample program

---
+++ src/WhileLoopExample.java
@@ -0,0 +1,9 @@
+public class WhileLoopExample {
+    public static void main(String[] args) {
+        int i = 1; // Initialization
+        while (i <= 5) { // Condition
+            System.out.println(i); // What happens in each loop
+            i++; // Iteration (make sure the loop eventually ends!)
+        }
+    }
+}
